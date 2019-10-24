b008af8181adcc9ee8e8df4bcf84e7fef3153229
dongdong-e <youngdong9800@gmail.com>
2019-10-24 18:56:15 +0900

    Basic of function
    componentDidMount()
    componentWillUnmount()
    componentDidUpdate()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 // React는 자동으로 class component의 render method를 실행함
 // class component는 class이지만 react component에서 확장 -> 스크린에 표시하기 위해서는 render 활용
@@ -18,9 +18,19 @@ class App extends React.Component {
     this.setState(current => ({ count: current.count - 1 }));
   };
 
+  componentDidMount() {
+    console.log("component rendered");
+  }
+
+  // componentWillUnmount(): component가 떠날 때 호출, 페이지 이동 등
+  componentWillUnmount() {
+    console.log("Goobye, cruel world.");
+  }
+
   // function(): 함수 바로 실행, function: 클릭할 때만 실행
   // function이 아니기 때문에 return이 적용되지 않음 -> render
   render() {
+    console.log("I'm rendering");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
@@ -28,6 +38,10 @@ class App extends React.Component {
         <button onClick={this.minus}>Minus</button>
       </div>
     );
+  }
+
+  componentDidUpdate() {
+    console.log("I just updated.");
   }
 }
 
