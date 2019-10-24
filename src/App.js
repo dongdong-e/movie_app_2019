ad0a814e22b7f009793f2fd3e2c0ea3339009d57
dongdong-e <youngdong9800@gmail.com>
2019-10-24 19:11:38 +0900

    Start fetching data
    Start fetching data

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
 import React from "react";
-// import PropTypes from "prop-types";
 
-// React는 자동으로 class component의 render method를 실행함
-// class component는 class이지만 react component에서 확장 -> 스크린에 표시하기 위해서는 render 활용
 class App extends React.Component {
-  // state는 object
   state = {
-    count: 0
+    isLoading: true
+    movies: []
   };
 
-  // state를 변경해주기 위해서는 setState를 활용해 render()을 호출함
-  // call setState, react is going to re-render!
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
-
+  // Called immediately after a component is mounted. Setting state here will trigger re-rendering.
   componentDidMount() {
-    console.log("component rendered");
+    // setTimeout(): delay function
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
 
-  // componentWillUnmount(): component가 떠날 때 호출, 페이지 이동 등
-  componentWillUnmount() {
-    console.log("Goobye, cruel world.");
-  }
-
-  // function(): 함수 바로 실행, function: 클릭할 때만 실행
-  // function이 아니기 때문에 return이 적용되지 않음 -> render
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
-  }
-
-  componentDidUpdate() {
-    console.log("I just updated.");
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
