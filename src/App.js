f2126a7c365f2db8bf6f410f2f894919e9c61147
dongdong-e <youngdong9800@gmail.com>
2019-10-25 16:07:21 +0900

    movie_app_2019
    movie_app_2019

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
-import { directive } from "@babel/types";
 import "./App.css";
 
 class App extends React.Component {
@@ -35,13 +34,13 @@ class App extends React.Component {
     // const { isLoading } = this.state; -> Get me this.state status.
     const { isLoading, movies } = this.state;
     return (
-      <section class="container">
+      <section className="container">
         {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">Loading...</span>
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
           </div>
         ) : (
-          <div class="movies">
+          <div className="movies">
             {movies.map(movie => (
               <Movie
                 key={movie.id}
@@ -50,6 +49,7 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
           </div>
