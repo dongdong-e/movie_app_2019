f52a1a8278e96fda7294b8f44415d13a57e3c905
dongdong-e <youngdong9800@gmail.com>
2019-10-25 16:27:49 +0900

    Updates

---
+++ precache-manifest.08a7ac7e353e3fcaf3e7ba7ff957ab28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10877c86f8ea1e8868b26b4d81ccae60",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "5df6733e9018f9ef12e8",
+    "url": "/movie_app_2019/static/css/main.f26c502f.chunk.css"
+  },
+  {
+    "revision": "2bed164abecb2001a4f8",
+    "url": "/movie_app_2019/static/js/2.14ba9984.chunk.js"
+  },
+  {
+    "revision": "5df6733e9018f9ef12e8",
+    "url": "/movie_app_2019/static/js/main.240cf209.chunk.js"
+  },
+  {
+    "revision": "44901d82ce6105999ea4",
+    "url": "/movie_app_2019/static/js/runtime-main.0f07e758.js"
+  }
+]);
